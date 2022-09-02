cdf33be13f393621be4846f0d97c07c450e75f47
muhammadnasif <nasif.imtiaz1997@gmail.com>
2022-09-02 23:14:51 +0600

    admin.py file updated

--- photographer/admin.py
+++ photographer/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(UserType)
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Photographer)
+admin.site.register(Photography_Package)
+admin.site.register(Order)
+admin.site.register(Portfolio)
