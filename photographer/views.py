3999ff6602df3941c9d84907b64c2f2fd349fa71
muhammadnasif <nasif.imtiaz1997@gmail.com>
2022-09-05 22:58:05 +0600

    photographer dashboard initialized

--- photographer/views.py
+++ photographer/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def load_test(request):
+
+    # logical code
+
+    # dictionary
+    context = {
+        'name': 'Rashid',
+        'age' : 25
+    }
+
+    # render paerge and send dictionary
+    return render(request, 'photographer/dashboard.html', context)
