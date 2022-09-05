9f66ad03d5012e1271f315e23b121f7598e8ba91
shadmanibnesaiful <shadmanibnesaiful@gmail.com>
2022-09-06 00:37:58 +0600

    upto ui initialization

--- Marriage_Moments/settings.py
+++ Marriage_Moments/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
--- photographer/views.py
+++ photographer/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
 
 # Create your views here.
 
 
 def load_test(request):
-
+    customer = Customer.objects.get(id=1)
     # logical code
 
     # dictionary
     context = {
         'name': 'Rashid',
-        'age' : 25
+        'age' : 25,
+        'customer' : customer
     }
 
     # render paerge and send dictionary
-    return render(request, 'photographer/dashboard.html', context)
+    return render(request, 'customer/index.html', context)
